d73622eb43e32779fa645716e67c4655f4a36c44
maisferramentas <maisferramentasmail@gmail.com>
2024-01-31 19:42:41 -0300

    Especificar versão do Python

--- ProjetoMaisFerramentas/settings.py
+++ ProjetoMaisFerramentas/settings.py
@@ -48,7 +48,9 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
+
 
 ROOT_URLCONF = 'ProjetoMaisFerramentas.urls'
 
@@ -126,6 +128,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 
